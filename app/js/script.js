6b7635f4f2b34c06adf94bfa6056b3db630113a7
Kristofer Wallöf <kristofer.wallof@spotonsolutions.se>
2017-03-04 23:31:49 +0100

    shadow mixin

--- app/js/script.js
+++ app/js/script.js
@@ -1,37 +1,16 @@
 (function() {
-  const container = document.querySelector(".products");
-  let items = container.querySelectorAll(".products__item");
-  let wrapper = container.querySelector(".products__wrapper");
-  let leftArrow = container.querySelector(".products__move--left");
-  let rightArrow = container.querySelector(".products__move--right");
+  let menuButton = document.querySelector('.header__button');
+  let content = document.querySelector('.content');
   
-  document.addEventListener("click", function (e) {
-    if(!e.target.classList.contains("products__item")) {
-      resetItems();
-    }
-  })
+  menuButton.addEventListener('click', toggleMenu);
   
-  function resetItems() {
-    
-    wrapper.style.transform = "translate(0)";
-    for (let item of items) {
-      item.classList.remove("active");
-      item.classList.remove("active__sibling");
+  function toggleMenu() {
+
+    if(content.className.includes('--active')) {
+       content.className="content";  
+    } else {
+      content.className="content" + "--active";
     }
   }
   
-  function changePosition(index) {
-    if(index === 0) {
-      wrapper.style.transform = "translate(10%)"; 
-      items[1].classList.add("active__sibling");
-    }
-  }
-  
-  for(let i = 0; i < items.length; i++) {
-    items[i].addEventListener("click", function(e) {
-      resetItems();
-      changePosition(i);
-      this.classList.add("active");
-    })
-  } 
-})();
+})(); 
